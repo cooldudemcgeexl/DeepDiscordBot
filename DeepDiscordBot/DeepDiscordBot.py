76d9a4bf49a20ef6932fce74c52b0fa607e43813
Nick Lay <layns@mail.uc.edu>
2020-06-17 23:56:46 -0400

    Improved documentation. Improved string parsing from CSV file.

--- DeepDiscordBot/DeepDiscordBot.py
+++ DeepDiscordBot/DeepDiscordBot.py
@@ -6,22 +6,22 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Loads user token for bot from .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 if TOKEN is None:
     print("No token found. Please check that the API token has been inserted into .env")
     exit(0)
 
-# client = discord.Client()
+
 bot = commands.Bot(command_prefix='!')
 
+# Initializing chance of message to 3%
 msgChanceUbound = 33
-
-
-
 
 @bot.command(name='change_freq', help = "Changes the frequency of this bot's messages", pass_context=True)
 async def changeFrequency(ctx, newFreq):
+    # Allows users to adjust frequency of messages sent by the bot
     global msgChanceUbound
     msgChanceUbound = int((1/int(newFreq))*100)
     print(f"Message chance changed to: {newFreq} %\n")
@@ -30,6 +30,7 @@ async def changeFrequency(ctx, newFreq):
 
 @bot.command(name='reload_knowledge', help = "Refreshes the bot's knowledge base")
 async def reload_CSV(ctx):
+    # Refreshes quote list. Useful if changing csv file without restarting bot. 
     global allquotes
     quotefile = open(file, "r", encoding = 'utf-8')
     allquotes = quotefile.readlines()
@@ -42,6 +43,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    # Writes to console/text file time, date, location, and contents of message sent by bot
     if message.author == bot.user:
         statusString = "[" + str(message.created_at) + "] Message sent in channel (" + message.channel.name + ") on server (" + message.channel.guild.name + "): " + message.content + "\n"
         print(statusString)
@@ -50,10 +52,14 @@ async def on_message(message):
         log_object.close()
         return
 
+    # Bot will pull a random message if mentioned.
     if bot.user.mentioned_in(message) and message.mention_everyone is False:
         await message.channel.send(pull_rand_csv())
         return
 
+
+    # Random chance between 1 and ubound to send a message.
+    # This causes bot to randomly interject when messages are sent.
     msgChance = random.randint(1,msgChanceUbound)
     if msgChance == 1:
         await message.channel.send(pull_rand_csv())
@@ -63,12 +69,12 @@ async def on_message(message):
 
 
 def pull_rand_csv():
+    # Reads a random line from the quote list and returns the message to be sent
     randline = random.randint(0,numrows)
     msgtext = allquotes[randline]
-    msgtext = msgtext.rstrip(',\n')
-    msgtext = msgtext.strip('\"')
 
-
+    # Removing quotes, comma, and newline. These will always occupy the same positions in the string.
+    msgtext = msgtext[1:-3]
     return msgtext
 
 def file_len(fname):
@@ -78,6 +84,8 @@ def file_len(fname):
     f.close()
     return i + 1
 
+
+# Initializing values for bot, this will be moved into proper structure later
 
 file = "outputtext.csv"
 numrows = file_len(file)
